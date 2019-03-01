09ebedb6e7cc591041ced0d9e8f2f0dc041350e2
GLAVAK <gau-gau@ya.ru>
2019-03-01 11:33:26 +0700

    Added project & Readme

---
+++ Assets/MouseFollow.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class MouseFollow : MonoBehaviour
+{
+    private void Start()
+    {
+    }
+
+    private void Update()
+    {
+        Vector3 point = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        transform.position = new Vector3(point.x, point.y, transform.position.z);
+    }
+}
---
+++ Assets/ToggleLight.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class ToggleLight : MonoBehaviour
+{
+    public AnimationCurve Intensity;
+
+    private float initInt;
+    private float rabd;
+
+    void Start()
+    {
+        initInt = GetComponent<Light>().intensity;
+        rabd = Random.Range(-5f, 1.5f);
+    }
+
+    private void Update()
+    {
+        GetComponent<Light>().intensity = Intensity.Evaluate(Time.time + rabd) + initInt;
+    }
+}
